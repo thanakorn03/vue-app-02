ed5d76cb40cb464e8a326bd87252e6553c7db9b2
thanakorn03 <iamok302@gmail.com>
2026-03-02 10:57:07 +0700

    fix config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-app-02/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
